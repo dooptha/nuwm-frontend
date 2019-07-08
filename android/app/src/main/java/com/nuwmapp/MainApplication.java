311575e92a01ce0440694e44f2c82d0a70890812
Vova Pihol <cetncat@gmail.com>
2019-07-08 18:38:43 +0300

    Fix Android build

--- android/app/src/main/java/com/nuwmapp/MainApplication.java
+++ android/app/src/main/java/com/nuwmapp/MainApplication.java
@@ -27,7 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
+      packages.add(new RNLocalizePackage());
+      packages.add(new RNGestureHandlerPackage());
+      packages.add(new RNScreensPackage());
       return packages;
     }
 

b04693778adaf9cb5ccf62b007feeaf5b9d73475
Vova Pihol <cetncat@gmail.com>
2019-07-10 17:36:16 +0300

    Remove npm from package.json, add config for builder to ignore usless folders

---
+++ rn-cli.config.js
@@ -0,0 +1,11 @@
+const blacklist = require('react-native/packager/blacklist');
+
+const config = {
+  getBlacklistRE(platform) {
+    return blacklist(platform, [
+      /node_modules\/build\/.idea\/.git\/.*/
+    ]);
+  }
+};
+
+module.exports = config;

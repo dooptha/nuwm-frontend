b1892326cc71eccfa78ba18d2d851616100da557
Andrew Pelykh <andrew.pelykh@gmail.com>
2019-08-01 12:23:40 +0300

    Get default locale from device

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -1,7 +1,32 @@
+import DeviceInfo from 'react-native-device-info';
 import { getKey } from './storage';
 
+
+const getDefaultLocale = () => {
+  const preferredLocales = DeviceInfo.getPreferredLocales();
+  let defaultLocale = 'en';
+
+  preferredLocales.some((locale) => {
+    switch (locale) {
+      case 'uk-UA':
+        defaultLocale = 'ua';
+        return true;
+      case 'ru-US':
+        defaultLocale = 'ru';
+        return true;
+      case 'en':
+        defaultLocale = 'en';
+        return true;
+      default:
+        return false;
+    }
+  });
+
+  return defaultLocale;
+};
+
 export const DEFAULT_PROPERTIES = {
-  language: 'ua',
+  language: getDefaultLocale(),
   theme: 'Eva Light',
   group: 'ПМ-41',
 };

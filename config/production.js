002ef74ab365f9ed5454663862605fa652f7d608
Andrew Pelykh <andrew.pelykh@gmail.com>
2019-08-05 07:42:31 +0300

    Add global exception handlers

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 export default {
-  API_ENDPOINT: "production_api",
-  SOCKET_IO_ENDPOINT: "production_api",
+  API_ENDPOINT: 'production_api',
+  SOCKET_IO_ENDPOINT: 'production_api',
   INITIAL_ROUTE_NAME: 'Settings', /* [Timetable, Conversations, Settings] */
   INSTAGRAM_HASHTAG: 'нувгп',
   TWITTER_HASHTAG: 'нувгп',
@@ -9,4 +9,9 @@ export default {
   GITHUB_URL: 'https://github.com/dooptha',
   DISCORD_URL: 'https://discord.gg/NEqGFEa',
   USE_DEFAULT_PROPERTIES: false,
+
+  // Exception handlers params
+  USE_CUSTOM_JS_EXCEPTION_HANDLER_IN_DEV: false,
+  NATIVE_EXCEPTION_HANDLER_FORCE_APP_QUIT: false,
+  NATIVE_EXCEPTION_HANDLER_EXECUTE_DEFAULT: false,
 };

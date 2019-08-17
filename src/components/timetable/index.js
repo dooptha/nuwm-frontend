6adebfb43954bb6890a89806ca03ada142cf71ee
Roman Sereda <roman.sereda@pm.me>
2019-08-17 11:34:41 +0300

    Stylize tab view

--- src/components/timetable/index.js
+++ src/components/timetable/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { ScrollView, RefreshControl } from 'react-native';
-import { Tab, TabView, withStyles } from 'react-native-ui-kitten';
+import { ScrollView, RefreshControl, Dimensions } from 'react-native';
+import { withStyles } from 'react-native-ui-kitten';
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 import Search from './search';
 import Schedule from './Schedule';
 
@@ -18,6 +19,13 @@ export class Timetable extends Component {
     selectedIndex: 3,
     refreshing: false,
     error: false,
+    index: 0,
+    routes: [
+      { key: 'first', title: I18n.t('timetable.tabs.Search') },
+      { key: 'second', title: I18n.t('timetable.tabs.Today') },
+      { key: 'third', title: I18n.t('timetable.tabs.Tomorrow') },
+      { key: 'fourth', title: I18n.t('timetable.tabs.Week') },
+    ],
   };
 
   componentDidMount() {
@@ -81,39 +89,73 @@ export class Timetable extends Component {
     );
   }
 
-  render() {
-    const { props: { themedStyle }, state: { data, selectedIndex } } = this;
-
+  renderScene = ({ route }) => {
+    const { data } = this.state;
     const schedule = replaceDatesWithMomentObjects(data);
 
     const today = schedule.filter((day) => isToday(day.date));
     const tomorrow = schedule.filter((day) => isTomorrow(day.date));
     const week = schedule.slice(0);
 
+    switch (route.key) {
+      case 'first':
+        return <Search />;
+      case 'second':
+        return this.renderSchedule(today, 1);
+      case 'third':
+        return this.renderSchedule(tomorrow, 2);
+      case 'fourth':
+        return this.renderSchedule(week, 3);
+      default:
+        return null;
+    }
+  };
+
+
+  render() {
+    const { props: { themedStyle }, state: { selectedIndex } } = this;
+
     return (
       <TabView
-        selectedIndex={selectedIndex}
-        onSelect={(index) => this.switchTab(index)}
-        style={themedStyle.tabView}
-      >
-        <Tab title={I18n.t('timetable.tabs.Search')}>
-          <Search />
-        </Tab>
-        <Tab title={I18n.t('timetable.tabs.Today')}>
-          { this.renderSchedule(today, 1) }
-        </Tab>
-        <Tab title={I18n.t('timetable.tabs.Tomorrow')}>
-          { this.renderSchedule(tomorrow, 2) }
-        </Tab>
-        <Tab title={I18n.t('timetable.tabs.Week')}>
-          { this.renderSchedule(week, 3) }
-        </Tab>
-      </TabView>
+        renderTabBar={(props) => (
+          <TabBar
+            style={themedStyle.tabBar}
+            labelStyle={themedStyle.tabBarLabel}
+            indicatorStyle={themedStyle.tabBarIndicator}
+            {...props}
+            activeColor={themedStyle.activeColor.color}
+            inactiveColor={themedStyle.inactiveColor.color}
+          />
+        )}
+        navigationState={this.state}
+        renderScene={this.renderScene}
+        onIndexChange={(index) => this.setState({ index })}
+        initialLayout={{ width: Dimensions.get('window').width }}
+      />
     );
   }
 }
 
 export default withStyles(Timetable, (theme) => ({
+  activeColor: {
+    color: theme['background-primary-color-1'],
+  },
+  inactiveColor: {
+    color: theme['text-hint-color'],
+  },
+  tabBarLabel: {
+    fontWeight: 'bold',
+    width: 'auto',
+    textTransform: 'capitalize',
+  },
+  tabBarIndicator: {
+    backgroundColor: theme['background-primary-color-1'],
+  },
+  tabBar: {
+    height: 40,
+    marginTop: -10,
+    backgroundColor: theme['background-basic-color-1'],
+  },
   tabView: {
     height: '100%',
     backgroundColor: theme['background-basic-color-1'],
